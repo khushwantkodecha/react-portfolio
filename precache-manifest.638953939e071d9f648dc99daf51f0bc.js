2036d142104cf6f45c36b0d4bec003f5e6517f47
Khushwant-CrewFoundry <khushwant.crewfoundry@gmail.com>
2020-08-04 13:47:22 +0530

    Updates

---
+++ precache-manifest.638953939e071d9f648dc99daf51f0bc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eaff4f7939e1d6adfc5770493c22655",
+    "url": "/khushwantkodecha.github.io/index.html"
+  },
+  {
+    "revision": "018a54c424b78dcc7584",
+    "url": "/khushwantkodecha.github.io/static/css/2.b0272f34.chunk.css"
+  },
+  {
+    "revision": "5c786d782bae0a3e3ba2",
+    "url": "/khushwantkodecha.github.io/static/css/main.5858d2a4.chunk.css"
+  },
+  {
+    "revision": "018a54c424b78dcc7584",
+    "url": "/khushwantkodecha.github.io/static/js/2.458f703a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/khushwantkodecha.github.io/static/js/2.458f703a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c786d782bae0a3e3ba2",
+    "url": "/khushwantkodecha.github.io/static/js/main.8a9c07fd.chunk.js"
+  },
+  {
+    "revision": "b73d4ba3581f78ad8100",
+    "url": "/khushwantkodecha.github.io/static/js/runtime-main.290252bc.js"
+  },
+  {
+    "revision": "3b4a31671e61770ede378ea593ab70e8",
+    "url": "/khushwantkodecha.github.io/static/media/cross4.3b4a3167.png"
+  },
+  {
+    "revision": "6bd08f435320e417c42d2b937217cb83",
+    "url": "/khushwantkodecha.github.io/static/media/hamburger3.6bd08f43.png"
+  },
+  {
+    "revision": "e1b7bd8ced5ab58da77b64c4fee49c48",
+    "url": "/khushwantkodecha.github.io/static/media/home7.e1b7bd8c.gif"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/khushwantkodecha.github.io/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/khushwantkodecha.github.io/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/khushwantkodecha.github.io/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/khushwantkodecha.github.io/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "528d29a0c11c2b6ffb3d5f9c7423b155",
+    "url": "/khushwantkodecha.github.io/static/media/profile.528d29a0.jpg"
+  }
+]);
